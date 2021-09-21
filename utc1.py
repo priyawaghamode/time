8bc3c758cd5ca5f50e901636257b3e1591a01c5d
priyawaghamode <priyawaghamode787@gmail.com>
2021-09-21 20:27:43 +0530

    utc time conversion

--- utc1.py
+++ utc1.py
@@ -1,9 +1,32 @@
 import pytz, datetime
+def check_user_input(input):
+    try:
+        # Convert it into integer
+        val = int(input)
+        print(val)
+    except ValueError:
+        try:
+            # Convert it into float
+            val = float(input)
+            print("No..Input is a float  number.It is not allowed Number = ", val)
+        except ValueError:
+            print("No.. input is not a number. It's a string")
+
 year=int(input("enter year(YYYY): "))
+check_user_input(year)
+
 month=int(input("enter month(MM): "))
+check_user_input(month)
+
 date=int(input("enter date(DD): "))
+check_user_input(date)
+
 hour=int(input("enter hour(HH): "))
+check_user_input(hour)
+
 minute=int(input("enter minutes(MM): "))
+check_user_input(minute)
+
 
 #convert to the date
 user_time=datetime.datetime(year,month,date,hour,minute)
@@ -14,7 +37,6 @@ cairo_timezone=pytz.timezone('Africa/Cairo')
 london_timezone=pytz.timezone('UTC')
 delhi_timezone=pytz.timezone('Asia/Kolkata')
 sydney_timezone=pytz.timezone('Australia/Sydney')
-
 UnitedKingdom_timezone=pytz.timezone('Europe/London')
 Switzerland_timezone=pytz.timezone('Europe/Zurich')
 SriLanka_timezone=pytz.timezone('Asia/Colombo')
@@ -27,7 +49,6 @@ cairo_time=pytz.utc.localize(user_time).astimezone(cairo_timezone)
 london_time=pytz.utc.localize(user_time).astimezone(london_timezone)
 delhi_time=pytz.utc.localize(user_time).astimezone(delhi_timezone)
 sydney_time=pytz.utc.localize(user_time).astimezone(sydney_timezone)
-
 UnitedKingdom_time=pytz.utc.localize(user_time).astimezone(UnitedKingdom_timezone)
 Switzerland_time=pytz.utc.localize(user_time).astimezone(Switzerland_timezone)
 SriLanka_time=pytz.utc.localize(user_time).astimezone(SriLanka_timezone)
@@ -41,7 +62,6 @@ print("\nCairo time is", cairo_time.isoformat())
 print("\nLondon time is", london_time.isoformat())
 print("\nDelhi time is", delhi_time.isoformat())
 print("\nSydney time is", sydney_time.isoformat())
-
 print("\nUnitedKingdom time is", UnitedKingdom_time.isoformat())
 print("\nSwitzerland time is", Switzerland_time.isoformat())
 print("\nSriLanka time is", SriLanka_time.isoformat())
